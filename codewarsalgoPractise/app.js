15790a816d78730da34b550c4342001d07b70132
Muhammad-Bilal-7896 <bilalmohib7896@gmail.com>
2021-07-15 11:00:00 +0100

    Completed the frieds list challenge

--- codewarsalgoPractise/app.js
+++ codewarsalgoPractise/app.js
@@ -2,15 +2,31 @@
 function reverseWords(str) {
     // Go for it
     let reversed;
-    let newArray=[];
+    let newArray = [];
     reversed = str.split(" ");
-    for(var i = 0;i<reversed.length; i++)
-    {
-        newArray.push(reversed[i].split("").reverse().join(""));       
+    for (var i = 0; i < reversed.length; i++) {
+        newArray.push(reversed[i].split("").reverse().join(""));
     }
     return newArray.join(" ");
 }
-let reversedString = reverseWords("This is an example!");
-console.log("This is the reversed string : ",reversedString);
+let reversedString = reverseWords("This is fun, hopefully.");
+console.log("This is the reversed string : ", reversedString);
+///////////////////////////////////////////////////////////////////////////////////////
+//2) Friend or Foe? 
+// Make a program that filters a list of strings and returns a list with only your friends name in it.
+// If a name has exactly 4 letters in it, you can be sure that it has to be a friend of yours! Otherwise, you can be sure he's not...
+// Ex: Input = ["Ryan", "Kieran", "Jason", "Yous"], Output = ["Ryan", "Yous"]
+// i.e.
+// friend ["Ryan", "Kieran", "Mark"] `shouldBe` ["Ryan", "Mark"]
 
-//2)  
+function friend(friends) {
+    let originalFriendsArray = [];
+    for (let i = 0; i < friends.length; i++) {
+        if (friends[i].length == 4) {
+            originalFriendsArray.push(friends[i]);
+        }
+    }
+    return originalFriendsArray;
+}
+let friendslist = friend(["Ryan", "Kieran", "Mark"]);
+console.log(`This is the friends list: ${friendslist}`)
