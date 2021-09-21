42beffe0d7f22aa2ee868cbf16cc8a2530e72d2e
Muhammad-Bilal-7896 <bilalmohib7896@gmail.com>
2021-09-22 01:35:17 +0500

    feat

--- Packt JS Workshop practise/index.js
+++ Packt JS Workshop practise/index.js
@@ -1,1 +1,15 @@
-console.log("Hello");
+//Clearing the console
+console.clear();
+
+//Printing hello
+console.log("Long Live Islam and Pakistan");
+
+var names = "bilal mohib";
+
+let other_name = "siddiqui";
+
+const dont_change = "This is constant";
+
+console.clear();
+console.log(`${names} ${other_name} ${dont_change}`);
+
--- js book practise/index.js
+++ js book practise/index.js
@@ -1,6 +1,10 @@
 //Adding in arrays
 let arr = [1,4,2,45,6,4,3,3];
+// arr.push(6);
+// console.log("Array : ",arr);
+//Adding in arrays
 
-arr.push(6);
-
+//Removing in arrays
+arr.shift();
 console.log("Array : ",arr);
+//Removing in arrays
