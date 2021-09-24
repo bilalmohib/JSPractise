903f85c8463f0767b3293540717398dea2c03ded
Muhammad-Bilal-7896 <bilalmohib7896@gmail.com>
2021-09-24 08:57:57 +0500

    push

--- w3schooljs/array/methods/pop.js
+++ w3schooljs/array/methods/pop.js
@@ -1,6 +1,10 @@
 //The Array pop method is there to remove the last element in an array
 let favourites = ["Allama Mukhtar Shah Naeemi Sahib","Mulana Abdul Habib Attari","Dr Ashraf Asif Jalali","Qasim Ali Shah Sahib","Remove It"];
 
-favourites.pop(); //The last element will be removed
+console.log("The Array before pop: ",favourites);
 
-console.log(favourites);
+let removed = favourites.pop(); //The last element will be removed
+
+console.log("The Array after pop: ",favourites);
+
+console.log("the removed element through pop is : ",removed);
---
+++ w3schooljs/array/methods/push.js
@@ -0,0 +1,11 @@
+//Push method is used to add the new element at the end of the array
+
+let arr = ['pakistan','afghanistan','china'];
+
+console.log("The arr element before push is : ",arr);
+
+let length = arr.push('India');
+
+console.log("The returned element from push is : ",length);
+
+console.log("The arr after push is : ",arr);
