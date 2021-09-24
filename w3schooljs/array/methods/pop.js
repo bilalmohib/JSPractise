c6c3eabb52b84b2c45bba4345ddb92cb3f0ec221
Muhammad-Bilal-7896 <bilalmohib7896@gmail.com>
2021-09-24 08:03:29 +0500

    pop

---
+++ w3schooljs/array/methods/pop.js
@@ -0,0 +1,6 @@
+//The Array pop method is there to remove the last element in an array
+let favourites = ["Allama Mukhtar Shah Naeemi Sahib","Mulana Abdul Habib Attari","Dr Ashraf Asif Jalali","Qasim Ali Shah Sahib","Remove It"];
+
+favourites.pop(); //The last element will be removed
+
+console.log(favourites);
